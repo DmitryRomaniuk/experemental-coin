73688ab1400b7223eea6e0d1b9670916b9fc4b79
Dzmitry Ramaniuk <kanumowa@gmail.com>
2017-08-25 00:15:38 +0300

    added release time, transfer

--- contracts/SellerContract.sol
+++ contracts/SellerContract.sol
@@ -5,20 +5,13 @@ import "node_modules/zeppelin-solidity/contracts/lifecycle/Destructible.sol";
 contract SellerContract is Destructible {
     mapping (address => uint) balances;
     mapping (address => string) votes;
+	uint releaseTime;
 
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	function SellerContract(address initialAccount, uint initialBalance) {
-		balances[initialAccount] = initialBalance;
-		totalSupply = initialBalance;
-	}
-
-	function sendCoin(address receiver, uint amount) returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		Transfer(msg.sender, receiver, amount);
-		return true;
+	function SellerContract(address _initialAccount, uint _initialBalance, uint _releaseTime) {
+		balances[_initialAccount] = _initialBalance;
+		releaseTime = _releaseTime;
 	}
 
 	function getBalanceInEth(address addr) returns(uint){
@@ -29,7 +22,27 @@ contract SellerContract is Destructible {
 		return balances[addr];
 	}
 
-    function transfer(address _to, uint256 _value) {
+    function donate(uint256 _value) returns(uint) {
+        require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[owner] += _value;
+        Transfer(msg.sender, owner, _value);
+        return true;
+    }
+
+    /**
+    * @dev transfer token for a specified address
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
+    function transfer(address _to, uint256 _value) internal returns (bool) { // should to be updated
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transfer(address _to, uint256 _value) { // should to be updated
         /* Check if sender has balance and for overflows */
         require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
         /* Add and subtract new balances */
@@ -37,34 +50,46 @@ contract SellerContract is Destructible {
         balanceOf[_to] += _value;
     }
 
-	function donate(uint256 _value) returns(uint) {
-        require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
-        balanceOf[msg.sender] -= _value;
-        balanceOf[owner] += _value;
-		Transfer(msg.sender, owner, _value);
-		return true;
+    /**
+    *
+    */
+    function propose(address addr) onlyOwner returns(uint) { // should to be updated
+        require(now >= releaseTime);
+
+        uint amount = token.balanceOf(this);
+        require(amount > 0);
+        token.transfer(beneficiary, amount);
+        return balances[addr];
+    }
+
+    /**
+    *
+    */
+    function getBackMoney(address addr) returns(uint) { // should to be updated
+        require(now >= releaseTime);
+        uint amount = token.balanceOf(this);
+        require(amount > 0);
+        token.transfer(beneficiary, amount);
+        return balances[addr];
+    }
+
+	function vote(string _value) external returns(string) {
+		votes[msg.sender] = strConcat(votes[msg.sender], " ");
+		votes[msg.sender] = strConcat(votes[msg.sender], _value);
 	}
 
-	function propose(address addr) returns(uint) {
-		return balances[addr];
-	}
-
-	function vote(address addr) returns(uint) {
-		return balances[addr];
+	function getVote(address addr) external constant returns (string) {
+		return votes[addr];
 	}
 
     function strConcat(string _a, string _b) internal returns (string){
-        bytes memory _ba = bytes(_a);
-        bytes memory _bb = bytes(_b);
-        string memory ab = new string(_ba.length + _bb.length);
-        bytes memory bytesab = bytes(ab);
+        bytes memory _bytesA = bytes(_a);
+        bytes memory _bytesB = bytes(_b);
+        string memory ab = new string(_bytesa.length + _bytesB.length);
+        bytes memory bytesAB = bytes(ab);
         uint k = 0;
-        for (uint i = 0; i < _ba.length; i++) bytesab[k++] = _ba[i];
-        for (i = 0; i < _bb.length; i++) bytesab[k++] = _bb[i];
-        return string(babcde);
-    }
-
-    function strConcat(string _a, string _b) internal returns (string) {
-        return strConcat(_a, _b);
+        for (uint i = 0; i < _bytesA.length; i++) bytesAB[k++] = _bytesA[i];
+        for (i = 0; i < _bytesB.length; i++) bytesAB[k++] = _bytesB[i];
+        return string(bytesAB);
     }
 }
