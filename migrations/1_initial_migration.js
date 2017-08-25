076c4918c114b29786315ce1c39d942c77395f0a
Dzmitry Ramaniuk2 <Dzmitry_Ramaniuk2@epam.com>
2017-08-25 18:13:10 +0300

    added migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,6 +1,11 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var MigrationsSeller = artifacts.require("./MigrationsSeller.sol");
 
 module.exports = function(deployer) {
   const tokenAmount = web3.toWei(1400000, "ether");
   deployer.deploy(Migrations, tokenAmount);
+  const tokenSeller = web3.toWei(0, "ether");
+  var date = Date.now();
+  date += date + 3600000;
+  deployer.deploy(MigrationsSeller, tokenSeller, date);
 };
